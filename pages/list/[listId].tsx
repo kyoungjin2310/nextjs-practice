6c00be9e80ded2c78f4b6e9372ef676c9b82baea
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-09-02 23:24:10 +0900

    feat: next js에서 서브메뉴 페이자 만드는 방법

---
+++ pages/list/[listId].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/common/layout";
+
+const ListDetail = () => {
+  return (
+    <Layout title={"List Detail"}>
+      <h1>blabla</h1>
+    </Layout>
+  );
+};
+
+export default ListDetail;
---
+++ pages/list/otherList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/common/layout";
+
+const OtherList = () => {
+  return (
+    <Layout title={"Other List"}>
+      <h1>blabla</h1>
+    </Layout>
+  );
+};
+
+export default OtherList;
