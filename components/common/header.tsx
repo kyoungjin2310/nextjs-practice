0b43c4544d2d1d58f9e0fd9f2e2a0e10d3b76b40
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-09-03 23:48:47 +0900

    feat: next js에서 link 쓰기

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
@@ -6,9 +7,15 @@ const Header = () => {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-            <a className="mr-5 hover:text-gray-900">Home</a>
-            <a className="mr-5 hover:text-gray-900">List</a>
-            <a className="mr-5 hover:text-gray-900">About</a>
+            <Link href="/">
+              <a className="mr-5 hover:text-gray-900">Home</a>
+            </Link>
+            <Link href="/list">
+              <a className="mr-5 hover:text-gray-900">List</a>
+            </Link>
+            <Link href="/about">
+              <a className="mr-5 hover:text-gray-900">About</a>
+            </Link>
           </nav>
           <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
             <svg
