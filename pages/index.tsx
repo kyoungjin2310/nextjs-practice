dd0b19432e45fbeadb171f708fae2a60d35624a0
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-09-07 23:38:11 +0900

    fix: api 오류 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Layout from "../components/common/layout";
 import Visual from "../components/main/visual";
-import { TOKEN, DATABASE_ID } from "../config/index";
-import { GetStaticProps, InferGetStaticPropsType, NextApiResponse } from "next";
 
 type Post = {
   author: string;
@@ -26,40 +24,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-export async function getServerSideProps() {
-  const options = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Notion-Version": "2022-06-28",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    },
-    body: JSON.stringify({
-      sorts: [
-        {
-          property: "Name",
-          direction: "ascending",
-        },
-      ],
-      page_size: 100,
-    }),
-  };
-
-  const res = await fetch(
-    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
-    options
-  );
-
-  const projects = await res.json();
-
-  const projectNames = projects.results?.map(
-    (aProject: any) => aProject.properties.Name.title[0].plain_text
-  );
-
-  console.log(projects);
-  return {
-    props: { projects },
-  };
-}
--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../../components/common/layout";
+import { TOKEN, DATABASE_ID } from "../../config/index";
 
 const List = () => {
   return (
@@ -10,3 +11,40 @@ const List = () => {
 };
 
 export default List;
+
+export async function getServerSideProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "Project",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  const projects = await res.json();
+
+  const projectNames = projects.results?.map(
+    (aProject: any) => aProject.properties.Project.title[0].plain_text
+  );
+
+  console.log(projectNames);
+  return {
+    props: { projects },
+  };
+}
