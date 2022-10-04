ff5df51eba5bc75c28cfbe18e3d297bd014441f6
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-05 00:06:36 +0900

    feat: recoil todolist 항목 추가하기

--- pages/list/otherList.tsx
+++ pages/list/otherList.tsx
@@ -1,43 +1,84 @@
-import React, { ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useRef } from "react";
 import Layout from "../../components/common/layout";
-import { atom, useRecoilState, selector, useRecoilValue } from "recoil";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
 
-const textState = atom({
-  key: "textState",
-  default: "",
+//type
+type atomArray<T> = Array<T>;
+
+type TodoItem = {
+  id: number;
+  text: string;
+  isComplete: boolean;
+};
+
+type TodoItemListProps = {
+  key: number;
+  item: string;
+};
+
+//atom
+const todoListState = atom<atomArray<TodoItem>>({
+  key: "todoListState",
+  default: [
+    {
+      id: 1,
+      text: "blablabla",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      text: "blablabla2222",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      text: "blablabla3333",
+      isComplete: false,
+    },
+  ],
 });
 
-const charCountState = selector({
-  key: "charCountState",
-  get: ({ get }) => {
-    const text = get(textState);
-    return text.length;
-  },
-});
-
-const OtherList = () => {
-  const [text, setText] = useRecoilState(textState);
-  const count = useRecoilValue(charCountState);
-  const onChange = (event: ChangeEvent) => {
-    setText((event.target as HTMLTextAreaElement).value);
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState("");
+  const setTodoList = useSetRecoilState<Array<TodoItem>>(todoListState);
+  const addItemId = useRef<number>(4);
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: addItemId.current + 1,
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue("");
   };
 
+  const onChange = (event: ChangeEvent) => {
+    setInputValue((event.target as HTMLTextAreaElement).value);
+  };
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
+
+const TodoItem = ({ key, item }: TodoItemListProps) => {
+  return <li>{item}</li>;
+};
+
+const OtherList = () => {
+  const todoList = useRecoilValue(todoListState);
   return (
     <Layout title={"Other List"}>
       <>
         <h1>blabla</h1>
-        <div>
-          <input
-            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-            placeholder="input change event"
-            type="text"
-            value={text}
-            onChange={onChange}
-          />
-          <br />
-          <p> Echo: {text}</p>
-          <p>Character Count: {count}</p>;
-        </div>
+        <TodoItemCreator />
+        {todoList.map((todoItem: TodoItem) => (
+          <TodoItem key={todoItem.id} item={todoItem.text} />
+        ))}
       </>
     </Layout>
   );
