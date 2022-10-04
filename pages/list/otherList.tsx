1f2f72f9558ad34d203209f050449775ec1c7574
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-04 23:16:41 +0900

    feat: recoil input change 연습

--- pages/list/otherList.tsx
+++ pages/list/otherList.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import Layout from "../../components/common/layout";
+import { atom, useRecoilState } from "recoil";
+
+const textState = atom({
+  key: "textState",
+  default: "",
+});
 
 const OtherList = () => {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (event: ChangeEvent) => {
+    setText((event.target as HTMLTextAreaElement).value);
+  };
+
   return (
     <Layout title={"Other List"}>
-      <h1>blabla</h1>
+      <>
+        <h1>blabla</h1>
+        <div>
+          <input
+            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            placeholder="input change event"
+            type="text"
+            value={text}
+            onChange={onChange}
+          />
+          <br />
+          Echo: {text}
+        </div>
+      </>
     </Layout>
   );
 };
