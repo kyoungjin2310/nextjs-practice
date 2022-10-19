2a3bc1b9c765507a5b2fc59ee7fa341d49a6ae73
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-19 23:57:17 +0900

    feat: recoil test

--- pages/list/test.tsx
+++ pages/list/test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { atom, selector, useRecoilValue } from "recoil";
+import { DATABASE_ID, TOKEN } from "../../config";
 
 const currentUserIDState = atom({
   key: "CurrentUserID",
@@ -10,6 +11,39 @@ const currentUserNameState = selector({
   key: "CurrentUserName",
   get: ({ get }) => {
     return "Alicia";
+  },
+});
+
+const currentUserNameQuery = selector({
+  key: "CurrentUserName",
+  get: async ({ get }) => {
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify({
+        sorts: [
+          {
+            property: "Project",
+            direction: "ascending",
+          },
+        ],
+        page_size: 100,
+      }),
+    };
+    const response = await fetch(
+      `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+      options
+    );
+
+    if (response.error) {
+      throw response.error;
+    }
+    return response.json();
   },
 });
 
